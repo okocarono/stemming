1ca959333a8372382f4450e2558b7a6db33c7c0e
Oko Carono <okocarono@gmail.com>
2019-07-02 18:19:36 +0700

    stemming

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+	$host='localhost';
+	$user='root';
+	$pass='';
+	$database='db_stemming-nazief';
+
+	$conn=mysql_connect($host,$user,$pass);
+	mysql_select_db($database);
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+	require_once ('stemming.php');
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>IR - STEMMING NAZIEF</title>
+ </head>
+ <body>
+ <center>
+ 	<h1>PENCARIAN KATA DASAR PADA BAHASA MELAYU<br> DENGAN ALGORITMA NAZIEF</h1>
+ 	<form method="post" action="">
+ 		<input type="text" name="kata" id="kata" size="20" value="<?php if(isset($_POST['kata'])){ echo $_POST['kata']; }else{ echo '';}?>">
+ 		<input type="submit" name="submit" value="Submit">
+ 	</form><br/>
+ 	<?php 
+ 		if(isset($_POST['kata'])){
+		$teksAsli = $_POST['kata'];
+		echo "Teks asli : ".$teksAsli.'<br/>';
+		$stemming = stemming($teksAsli);
+		echo "Kata dasar : ".$stemming.'<br/>';
+		}
+ 	 ?></center>
+ </body>
+ </html>
